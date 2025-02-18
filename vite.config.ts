b61e46c4a323d6d4d64b1ee6b5c306449b97e514
greenridersahmedabad <greenriders.ahmedabad@gmail.com>
2025-02-18 12:39:59 +0530

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/green-riders-website/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/green-riders-website/' : '/',
+  base: '/',
   define: {
     __APP_VERSION__: JSON.stringify(process.env.VITE_GIT_COMMIT_ID || 'dev'),
   },
