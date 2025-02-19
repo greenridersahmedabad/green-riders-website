52a23fd134e1635c2883ae8014fe63da1e4ba639
greenridersahmedabad <greenriders.ahmedabad@gmail.com>
2025-02-19 17:53:59 +0530

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/green-riders-website/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/green-riders-website/' : '/',
   // base: '/',
   define: {
     __APP_VERSION__: JSON.stringify(process.env.VITE_GIT_COMMIT_ID || 'dev'),
