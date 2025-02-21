7a9c8bfccf3dab3f700aab44eb00674d9a07f439
greenridersahmedabad <greenriders.ahmedabad@gmail.com>
2025-02-21 15:11:43 +0530

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import SEO from "./components/SEO.tsx";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
+      <Analytics />
       <SEO />
       <App />
     </HelmetProvider>
