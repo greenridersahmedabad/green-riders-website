285267469b553114dad475bf4c65a10f590d0790
greenridersahmedabad <greenriders.ahmedabad@gmail.com>
2025-02-28 19:06:38 +0530

    Added Event

--- src/content/events.tsx
+++ src/content/events.tsx
@@ -1,12 +1,14 @@
-export const events =  [
+import GRGP2025 from "../images/blogs/GRGP2025/GRGP2025.jpg"
+
+export const events = [
     {
-        id: 'mountain-challenge',
-        title: "Mountain Challenge",
-        date: "February 28, 2024",
-        location: "Mount Green Peak",
-        distance: "45km",
-        participants: 15,
-        image: "https://images.unsplash.com/photo-1534787238916-9ba6764efd4f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-        description: "An exhilarating 45km ride through the breathtaking trails of Mount Green Peak. Experience nature at its finest and test your endurance against rugged terrain."
-      }
-  ];
+        id: 'mountain-parikrama',
+        title: "Girnar Cycling Parikrama",
+        date: "January 5, 2025",
+        location: "Mount Girnar Junagadh",
+        distance: "73km",
+        participants: 210,
+        image: GRGP2025,
+        description: "Experience the thrill of cycling around the majestic Mount Girnar with the Girnar Cycling Parikrama! Covering 73 km through scenic routes, historic sites, and lush landscapes, this ride brings together 210 passionate cyclists to celebrate endurance, adventure, and the rich heritage of Junagadh. Join us on January 5, 2025, for an unforgettable journey around one of Gujarat’s most revered mountains! "
+    }
+];
