744f8e1203fc5951faa3ad839435b6e1546982bc
greenridersahmedabad <greenriders.ahmedabad@gmail.com>
2025-03-01 14:56:49 +0530

    Events added

--- src/content/events.tsx
+++ src/content/events.tsx
@@ -1,4 +1,5 @@
-import GRGP2025 from "../images/blogs/GRGP2025/GRGP2025.jpg"
+import { GRGP2025, Heritage2024 } from "../images/blogs"
+
 
 export const events = [
     {
@@ -9,6 +10,16 @@ export const events = [
         distance: "73km",
         participants: 210,
         image: GRGP2025,
-        description: "Experience the thrill of cycling around the majestic Mount Girnar with the Girnar Cycling Parikrama! Covering 73 km through scenic routes, historic sites, and lush landscapes, this ride brings together 210 passionate cyclists to celebrate endurance, adventure, and the rich heritage of Junagadh. Join us on January 5, 2025, for an unforgettable journey around one of Gujarat’s most revered mountains! "
+        description: `GRGP2025 – Green Riders Girnar Parikrama 2025. History Created! Over 200 cyclists from Gujarat and Mumbai converged for a record-breaking 73 km cycling parikrama around the majestic Mount Girnar! This epic spiritual endurance ride took cyclists through Scenic routes, Quaint villages and Lush landscapes. Uniting cycling groups from various cities, the Green Riders Girnar Parikrama 2025 created an unforgettable experience in the historic city of Junagadh. A testament to human endurance, camaraderie, and the love of cycling!`
+    },
+    {
+        id: 'ahm-wall-city-ride',
+        title: "Heirtage Ride-1",
+        date: "May 3, 2024",
+        location: "Old City Ahmedabad",
+        distance: "20 km",
+        participants: 15,
+        image: Heritage2024,
+        description: `Discovering Hidden Gems: 20km Ahmedabad Walled City Heritage Ride We explored the vibrant streets of our own UNESCO World Heritage City! 20 Km Walled City Ride covered Key landmarks: Lakkhadiyo Bridge, Kalupur Swaminarayan Mandir, Poet Dalpatram Chowk, Kalupur Darwaja, Prem Darwaja, Dariyapur Darwaja, Delhi Darwaja, Hathising ni vadi. Special treat - Savoring traditional Navkarsi breakfast at the historic Hathising Dera! A memorable ride through Ahmedabad's rich cultural heritage!`
     }
 ];
---
+++ src/images/blogs/index.tsx
@@ -0,0 +1,8 @@
+import GRGP2025 from "./GRGP2025/GRGP2025.jpg"
+import Heritage2024 from "./Heritage2024/heritage2024.jpg"
+
+
+export {
+    GRGP2025,
+    Heritage2024
+}
